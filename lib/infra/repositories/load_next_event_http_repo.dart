6b7af6f8130a49121869d609698a5902114dc228
Kauan Curbani <crbnii2020@gmail.com>
2024-08-14 15:26:18 -0300

    move repository to own file

---
+++ lib/infra/repositories/load_next_event_http_repo.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:http/http.dart';
+
+class LoadNextEventHttpRepository {
+  final Client httpClient;
+  final String url;
+  final Map<String, String> _headers = {
+    "content-type": "application/json",
+    "accept": "application/json",
+  };
+
+  LoadNextEventHttpRepository({required this.httpClient, required this.url});
+
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
+    final response = await httpClient.get(
+        Uri.parse(url.replaceFirst(":groupId", groupId)),
+        headers: _headers);
+
+    final event = jsonDecode(response.body);
+
+    return NextEvent(
+      groupName: event["groupName"],
+      date: DateTime.parse(event["date"]),
+      players: (event["players"] as List)
+          .map((player) => NextEventPlayer(
+                id: player["id"],
+                name: player["name"],
+                isConfirmed: player["isConfirmed"],
+                confirmationDate:
+                    DateTime.tryParse(player["confirmationDate"] ?? ""),
+                photo: player["photo"],
+                position: player["position"],
+              ))
+          .toList(),
+    );
+  }
+}
--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -2,45 +2,11 @@ import 'dart:convert';
 
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:advanced_flutter/infra/repositories/load_next_event_http_repo.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
-
-class LoadNextEventHttpRepository {
-  final Client httpClient;
-  final String url;
-  final Map<String, String> _headers = {
-    "content-type": "application/json",
-    "accept": "application/json",
-  };
-
-  LoadNextEventHttpRepository({required this.httpClient, required this.url});
-
-  Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final response = await httpClient.get(
-        Uri.parse(url.replaceFirst(":groupId", groupId)),
-        headers: _headers);
-
-    final event = jsonDecode(response.body);
-
-    return NextEvent(
-      groupName: event["groupName"],
-      date: DateTime.parse(event["date"]),
-      players: (event["players"] as List)
-          .map((player) => NextEventPlayer(
-                id: player["id"],
-                name: player["name"],
-                isConfirmed: player["isConfirmed"],
-                confirmationDate:
-                    DateTime.tryParse(player["confirmationDate"] ?? ""),
-                photo: player["photo"],
-                position: player["position"],
-              ))
-          .toList(),
-    );
-  }
-}
 
 class MockClient extends Mock implements Client {}
 
