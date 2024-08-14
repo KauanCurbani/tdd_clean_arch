bdae6afc3b8c0dce0fbbb14c1736c19b5e03d11d
Kauan Curbani <crbnii2020@gmail.com>
2024-08-14 14:38:45 -0300

    added test to check correct url

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -5,11 +5,12 @@ import 'package:mocktail/mocktail.dart';
 
 class LoadNextEventHttpRepository {
   final Client httpClient;
+  final String url;
 
-  LoadNextEventHttpRepository({required this.httpClient});
+  LoadNextEventHttpRepository({required this.httpClient, required this.url});
 
   Future<void> loadNextEvent({required String groupId}) async {
-    await httpClient.get(Uri.parse("https://any-url.com"));
+    await httpClient.get(Uri.parse(url.replaceFirst(":groupId", groupId)));
   }
 }
 
@@ -20,20 +21,26 @@ class FakeUri extends Fake implements Uri {}
 void main() {
   late String groupId;
   late Client client;
+  late LoadNextEventHttpRepository sut;
+  const url = "https://any-url.com/api/v1/groups/:groupId/next-event";
 
   setUp(() {
     groupId = Faker().guid.guid();
     client = MockClient();
-
+    sut = LoadNextEventHttpRepository(httpClient: client, url: url);
     when(() => client.get(any())).thenAnswer((_) async => Response("", 200));
   });
 
   setUpAll(() => {registerFallbackValue(FakeUri())});
 
   test("should request with correct method", () async {
-    final sut = LoadNextEventHttpRepository(httpClient: client);
     await sut.loadNextEvent(groupId: groupId);
-
     verify(() => client.get(any())).called(1);
+  });
+
+  test("should request with correct url", () async {
+    await sut.loadNextEvent(groupId: groupId);
+    Uri expectedUri = Uri.parse(url.replaceFirst(":groupId", groupId));
+    verify(() => client.get(expectedUri)).called(1);
   });
 }
