6c70b33810c908420477d82696e3296aef4ff239
Kauan Curbani <crbnii2020@gmail.com>
2024-08-15 08:23:56 -0300

    added implementation to query strings on httpClient

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -13,21 +13,28 @@ class HttpClient {
   HttpClient({required this.client});
 
   Future<void> get(String url,
-      {Map<String, String>? headers, Map<String, String?>? params}) async {
+      {Map<String, String>? headers,
+      Map<String, String?>? params,
+      Map<String, String>? qs}) async {
     var allHeaders = (headers ?? {})..addAll(_defaultHeaders);
-    Uri uri = _buildUri(url: url, params: params);
+    Uri uri = _buildUri(url: url, params: params, qs: qs);
     await client.get(uri, headers: allHeaders);
   }
 
-  Uri _buildUri({required String url, Map<String, String?>? params}) {
+  Uri _buildUri(
+      {required String url,
+      Map<String, String?>? params,
+      Map<String, String>? qs}) {
     if (params != null) {
       params.forEach(
         (key, value) => url = url.replaceFirst(":$key", value ?? ""),
       );
     }
+    if (qs != null) {
+      url += "?${qs.entries.map((e) => "${e.key}=${e.value}").join("&")}";
+    }
 
     if (url.endsWith("/")) url = url.substring(0, url.length - 1);
-
     return Uri.parse(url);
   }
 }
@@ -89,8 +96,10 @@ void main() {
 
       await sut.get(url, params: {"id": "value", "key2": "value2"});
 
-      verify(() => client.get(Uri.parse("https://any-url.com/api/value/value2"),
-          headers: any(named: "headers"))).called(1);
+      verify(() => client.get(
+            Uri.parse("https://any-url.com/api/value/value2"),
+            headers: any(named: "headers"),
+          )).called(1);
     });
 
     test("should request with optional params", () async {
@@ -98,8 +107,10 @@ void main() {
 
       await sut.get(url, params: {"id": "value", "key2": null});
 
-      verify(() => client.get(Uri.parse("https://any-url.com/api/value"),
-          headers: any(named: "headers"))).called(1);
+      verify(() => client.get(
+            Uri.parse("https://any-url.com/api/value"),
+            headers: any(named: "headers"),
+          )).called(1);
     });
 
     test("should request with invalid params", () async {
@@ -107,8 +118,30 @@ void main() {
 
       await sut.get(url, params: {"id": "value"});
 
-      verify(() => client.get(Uri.parse("https://any-url.com/api/value/:key2"),
-          headers: any(named: "headers"))).called(1);
+      verify(() => client.get(
+            Uri.parse("https://any-url.com/api/value/:key2"),
+            headers: any(named: "headers"),
+          )).called(1);
+    });
+
+    test("should request with correct queryStrings", () async {
+      await sut.get(url, qs: {"id": "value", "key2": "value2"});
+
+      verify(() => client.get(
+            Uri.parse("$url?id=value&key2=value2"),
+            headers: any(named: "headers"),
+          )).called(1);
+    });
+
+    test("should request with correct queryStrings and params", () async {
+      url = "https://any-url.com/api/:id";
+
+      await sut.get(url, params: {"id": "value"}, qs: {"key2": "value2"});
+
+      verify(() => client.get(
+            Uri.parse("https://any-url.com/api/value?key2=value2"),
+            headers: any(named: "headers"),
+          )).called(1);
     });
   });
 }
