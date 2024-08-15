5594c98b0cb2a021f33ccb2e2240b3e3a08df841
Kauan Curbani <crbnii2020@gmail.com>
2024-08-15 14:18:54 -0300

    ensure NextEventPage hide circularProgressIndicator on success

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -1,6 +1,9 @@
+import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:rxdart/rxdart.dart';
 
 final class NextEventPage extends StatefulWidget {
   final NextEventPresenter presenter;
@@ -20,12 +23,23 @@ class _NextEventPageState extends State<NextEventPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: widget.presenter.nextEventStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.active) {
+            return const CircularProgressIndicator();
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
 
 abstract class NextEventPresenter {
   void load(String groupId);
+  Stream get nextEventStream;
 }
 
 final class NextEventPresenterMock with Mock implements NextEventPresenter {}
@@ -34,6 +48,7 @@ void main() {
   late NextEventPresenter presenterMock;
   late Widget sut;
   late String groupId;
+  late BehaviorSubject nextEventSubject;
 
   Widget makeSut() {
     return MaterialApp(
@@ -46,13 +61,30 @@ void main() {
 
   setUp(() {
     presenterMock = NextEventPresenterMock();
-    groupId = "any_group_id";
+    groupId = Faker().guid.guid();
     sut = makeSut();
+    nextEventSubject = BehaviorSubject();
+
     when(() => presenterMock.load(any())).thenAnswer((_) async {});
+    when(() => presenterMock.nextEventStream).thenAnswer((_) => nextEventSubject.stream);
   });
 
   testWidgets("should load event data on page init", (WidgetTester tester) async {
     await tester.pumpWidget(sut);
     verify(() => presenterMock.load(groupId)).called(1);
   });
+  testWidgets("should present spinner when data is loading", (WidgetTester tester) async {
+    await tester.pumpWidget(sut);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+  testWidgets("should hide spinner on loading success", (WidgetTester tester) async {
+    await tester.pumpWidget(sut);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    nextEventSubject.add(NextEvent(groupName: "Group Name", date: DateTime.now(), players: []));
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  
 }
