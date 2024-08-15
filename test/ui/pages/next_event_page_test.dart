78e4e88cd6bb16f5279ebe825036624e33047315
Kauan Curbani <crbnii2020@gmail.com>
2024-08-15 16:06:54 -0300

    move nextEventPage to its own file

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -1,72 +1,12 @@
 import 'package:advanced_flutter/presentation/presenters/next_event_presenter.dart';
+import 'package:advanced_flutter/ui/pages/next_event_page.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rxdart/rxdart.dart';
 
-final class NextEventPage extends StatefulWidget {
-  final NextEventPresenter presenter;
-  final String groupId;
-  const NextEventPage({super.key, required this.presenter, required this.groupId});
 
-  @override
-  State<NextEventPage> createState() => _NextEventPageState();
-}
-
-class _NextEventPageState extends State<NextEventPage> {
-  @override
-  void initState() {
-    super.initState();
-    widget.presenter.load(widget.groupId);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<NextEventViewModel>(
-        stream: widget.presenter.nextEventStream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.active) {
-            return const CircularProgressIndicator();
-          }
-          if (snapshot.hasError) {
-            return const Text("Erro ao carregar os dados");
-          }
-
-          final viewModel = snapshot.data!;
-
-          return ListView(
-            children: [
-              if (viewModel.goalkeepers.isNotEmpty)
-                ListSection(title: "DENTRO - GOLEIROS", items: viewModel.goalkeepers),
-              if (viewModel.players.isNotEmpty) ListSection(title: "DENTRO - JOGADORES", items: viewModel.players),
-              if (viewModel.out.isNotEmpty) ListSection(title: "FORA", items: viewModel.out),
-              if (viewModel.doubt.isNotEmpty) ListSection(title: "DÚVIDA", items: viewModel.doubt),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}
-
-final class ListSection extends StatelessWidget {
-  final String title;
-  final List<NextEventPlayerViewModel> items;
-  const ListSection({super.key, required this.title, required this.items});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(title),
-        Text(items.length.toString()),
-        ...items.map((player) => Text(player.name)),
-      ],
-    );
-  }
-}
 
 final class NextEventPresenterMock with Mock implements NextEventPresenter {}
 
