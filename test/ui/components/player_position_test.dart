d6e154a4a4d035eb043f01c2d349e384024f1ff9
Kauan Curbani <crbnii2020@gmail.com>
2024-08-15 16:18:17 -0300

    added player position component

---
+++ test/ui/components/player_position_test.dart
@@ -0,0 +1,30 @@
+import 'package:advanced_flutter/ui/components/player_position.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("should handle goalkeeper position", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerPosition(position: "goalkeeper")));
+    expect(find.text("Goleiro"), findsOneWidget);
+  });
+
+  testWidgets("should handle defender position", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerPosition(position: "defender")));
+    expect(find.text("Zagueiro"), findsOneWidget);
+  });
+
+  testWidgets("should handle midfielder position", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerPosition(position: "midfielder")));
+    expect(find.text("Meia"), findsOneWidget);
+  });
+
+  testWidgets("should handle forward position", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerPosition(position: "forward")));
+    expect(find.text("Atacante"), findsOneWidget);
+  });
+
+  testWidgets("should handle position less", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerPosition(position: null)));
+    expect(find.text("Gandula"), findsOneWidget);
+  });
+}
